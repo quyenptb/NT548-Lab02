21fe20a98cf1755a0077c3770ca9bea1d3a27e9d
quyenptb <phanphan7a2@gmail.com>
2025-12-24 07:41:25 +0000

    update .gitignore

--- app/app.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Hello DevOps Lab 02!'));
-app.listen(3000, () => console.log('App running on port 3000'));
